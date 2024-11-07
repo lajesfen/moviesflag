fada53f2ea77588fbf5c3d58e329cd13f801bfa9
lajesfen <luciano.aguirre@utec.edu.pe>
2024-11-07 14:12:09 -0500

    add: Mem. Cache, Pagination & Thread Executor

--- app.py
+++ app.py
@@ -1,71 +1,111 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import json
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
-apikey = "<apikey>"
+apikey = "9ad2644b"
+
+cache = {
+    "movieSearch": {},
+    "movieDetails": {},
+    "countryFlags": {}
+}
 
 def searchfilms(search_text):
-    url = "https://www.omdbapi.com/?s=" + search_text + "&apikey=" + apikey
+    if search_text in cache["movieSearch"]:
+        print("Found search results for", search_text, "in cache")
+        return cache["movieSearch"][search_text]
+
+    url = f"https://www.omdbapi.com/?s={search_text}&apikey={apikey}"
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
-    else:
-        print("Failed to retrieve search results.")
-        return None
-    
-def getmoviedetails(movie):
-    url = "https://www.omdbapi.com/?i=" + movie["imdbID"] + "&apikey=" + apikey
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
+        results = response.json()
+        cache["movieSearch"][search_text] = results
+        print("Calling Search API")
+        return results
     else:
         print("Failed to retrieve search results.")
         return None
 
-def get_country_flag(fullname):
+def getmoviedetails(imdb_id):
+    if imdb_id in cache["movieDetails"]:
+        print("Found movie details for", imdb_id, "in cache")
+        return cache["movieDetails"][imdb_id]
 
-    url = f"https://restcountries.com/v3.1/name/{fullname}?fullText=true"
+    url = f"https://www.omdbapi.com/?i={imdb_id}&apikey={apikey}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = response.json()
+        entry = {
+            "imdbID": result["imdbID"],
+            "Title": result["Title"],
+            "Year": result["Year"],
+            "Country": result["Country"]
+        }
+        cache["movieDetails"][imdb_id] = entry
+        print("Calling Movie API for", imdb_id)
+        return entry
+    else:
+        print("Failed to retrieve movie details.")
+        return None
+
+def get_country_flag(country_name):
+    if country_name in cache["countryFlags"]:
+        print("Found flag for", country_name, "in cache")
+        return cache["countryFlags"][country_name]
+
+    url = f"https://restcountries.com/v3.1/name/{country_name}?fullText=true"
     response = requests.get(url)
     if response.status_code == 200:
         country_data = response.json()
         if country_data:
-            return country_data[0].get("flags", {}).get("svg", None)
-    print(f"Failed to retrieve flag for country code: {fullname}")
+            flag_url = country_data[0].get("flags", {}).get("svg", None)
+            cache["countryFlags"][country_name] = flag_url
+            print("Calling Flag API for", country_name)
+            return flag_url
+    print("Failed to retrieve flag for country:", country_name)
     return None
 
-def merge_data_with_flags(filter):
-    filmssearch = searchfilms(filter)
-    moviesdetailswithflags = []
-    for movie in filmssearch["Search"]:
-         moviedetails = getmoviedetails(movie)
-         countriesNames = moviedetails["Country"].split(",")
-         countries = []
-         for country in countriesNames:
-            countrywithflag = {
-                "name": country.strip(),
-                "flag": get_country_flag(country.strip())
-            }
-            countries.append(countrywithflag)
-         moviewithflags = {
-            "title": moviedetails["Title"],
-            "year": moviedetails["Year"],
-            "countries": countries
-         }
-         moviesdetailswithflags.append(moviewithflags)
+def merge_data_with_flags(filter, page, page_limit):
+    film_search = searchfilms(filter)
 
-    return moviesdetailswithflags
+    start_index = (page - 1) * page_limit
+    end_index = start_index + page_limit
+    paginated_movies = film_search['Search'][start_index:end_index]
+
+    movies_details_with_flags = []
+    with ThreadPoolExecutor() as executor:
+        movies_details = list(executor.map(lambda movie: getmoviedetails(movie["imdbID"]), paginated_movies))
+        
+        for movie in movies_details:
+            country_names = movie["Country"].split(", ")
+            countries = [{"name": name, "flag": get_country_flag(name)} for name in country_names]
+            movies_details_with_flags.append({
+                "title": movie["Title"],
+                "year": movie["Year"],
+                "countries": countries
+            })
+
+    return movies_details_with_flags
 
 @app.route("/")
 def index():
     filter = request.args.get("filter", "").upper()
-    return render_template("index.html", movies = merge_data_with_flags(filter))
+    page = int(request.args.get("page", 1))
+    page_limit = int(request.args.get("page_limit", 5))
+    movies = merge_data_with_flags(filter, page, page_limit)
+    return render_template("index.html", movies=movies)
 
 @app.route("/api/movies")
 def api_movies():
-    filter = request.args.get("filter", "")
-    return jsonify(merge_data_with_flags(filter))    
+    filter = request.args.get("filter", "").upper()
+    page = int(request.args.get("page", 1))
+    page_limit = int(request.args.get("page_limit", 5))
+    return jsonify(merge_data_with_flags(filter, page, page_limit))
+
+@app.route("/cache")
+def cache_data():
+    return jsonify(cache)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
